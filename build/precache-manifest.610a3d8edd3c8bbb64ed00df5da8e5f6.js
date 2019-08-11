ecb73d735ddb215b91f7e76741e905fa6dbe6e03
Warren <warrenwwang@berkeley.edu>
2019-08-11 15:24:05 -0700

    reorganized and cleaned up icons

---
+++ build/precache-manifest.610a3d8edd3c8bbb64ed00df5da8e5f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e4e51c139c726ebae6c49e3302fb18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5ae77958b438ec89e7b",
+    "url": "/static/css/main.ba741746.chunk.css"
+  },
+  {
+    "revision": "f685184b34714169f24c",
+    "url": "/static/js/2.63ef2da0.chunk.js"
+  },
+  {
+    "revision": "d5ae77958b438ec89e7b",
+    "url": "/static/js/main.c2de85bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c20eeb3e4cf6dfdbcde6dcb008fd1a97",
+    "url": "/static/media/codebase_icon.c20eeb3e.png"
+  },
+  {
+    "revision": "ae856ccffc9b2891c9e9c06fd327f3f4",
+    "url": "/static/media/ginger_icon1.ae856ccf.png"
+  },
+  {
+    "revision": "c9ee7827ab3851b6cf683baca6a06049",
+    "url": "/static/media/warren.c9ee7827.JPG"
+  }
+]);
