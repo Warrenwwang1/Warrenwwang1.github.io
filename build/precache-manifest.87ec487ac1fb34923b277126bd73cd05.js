6c50c0d93142293992c4b5033869934f7141c868
Warren <warrenwwang@berkeley.edu>
2019-09-11 18:03:00 -0700

    trying something

---
+++ build/precache-manifest.87ec487ac1fb34923b277126bd73cd05.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc85ff851e2e73d33c254dea941531bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e341999e1ebc3583998e",
+    "url": "/static/css/main.719aa0e6.chunk.css"
+  },
+  {
+    "revision": "167ce2f58f560e76b6be",
+    "url": "/static/js/2.d3853680.chunk.js"
+  },
+  {
+    "revision": "e341999e1ebc3583998e",
+    "url": "/static/js/main.148982f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "11d2d73359a3c6482fd6ad125cb24483",
+    "url": "/static/media/basketball.11d2d733.JPG"
+  },
+  {
+    "revision": "5e48a10ece86b6423eb935ef3c0feb8c",
+    "url": "/static/media/bogosort.5e48a10e.png"
+  },
+  {
+    "revision": "c20eeb3e4cf6dfdbcde6dcb008fd1a97",
+    "url": "/static/media/codebase_icon.c20eeb3e.png"
+  },
+  {
+    "revision": "897b581e37505481ad106b23828459d9",
+    "url": "/static/media/daryn.897b581e.jpg"
+  },
+  {
+    "revision": "e8565b760cde74def5574cbe62043e6b",
+    "url": "/static/media/email.e8565b76.png"
+  },
+  {
+    "revision": "ae856ccffc9b2891c9e9c06fd327f3f4",
+    "url": "/static/media/ginger_icon1.ae856ccf.png"
+  },
+  {
+    "revision": "24296e4a3e0fd43675965a24f4c4ba05",
+    "url": "/static/media/jazz.24296e4a.JPG"
+  },
+  {
+    "revision": "68692a031f19a75a1670d83e81b51ec6",
+    "url": "/static/media/piratebaby.68692a03.png"
+  },
+  {
+    "revision": "7a1285cf9a18dbe8719cd161c0ac07fc",
+    "url": "/static/media/resume.7a1285cf.png"
+  },
+  {
+    "revision": "851746db3831b030f367a17a83b563ed",
+    "url": "/static/media/swapi.851746db.png"
+  },
+  {
+    "revision": "c9ee7827ab3851b6cf683baca6a06049",
+    "url": "/static/media/warren.c9ee7827.JPG"
+  }
+]);
